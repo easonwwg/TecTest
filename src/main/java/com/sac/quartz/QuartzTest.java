6ac6e56aa53d33cb01e2875c367fedb7ece1c168
easonwwg <990793233@qq.com>
2018-01-16 16:29:28 +0800

    工具测试

--- src/main/java/com/sac/quartz/QuartzTest.java
+++ src/main/java/com/sac/quartz/QuartzTest.java
@@ -9,6 +9,7 @@ import org.quartz.impl.StdSchedulerFactory;
 import static org.quartz.JobBuilder.newJob;
 import static org.quartz.SimpleScheduleBuilder.simpleSchedule;
 import static org.quartz.TriggerBuilder.newTrigger;
+
 /**
  * @author:eason
  * @Description：
@@ -23,17 +24,26 @@ public class QuartzTest {
             Scheduler scheduler = StdSchedulerFactory.getDefaultScheduler();
 
             //定义一个Trigger
-            Trigger trigger = newTrigger().withIdentity("trigger1", "group1") //定义name/group
-                    .startNow()//一旦加入scheduler，立即生效
-                    .withSchedule(simpleSchedule() //使用SimpleTrigger
-                            .withIntervalInSeconds(6) //每隔一秒执行一次
-                            .repeatForever()) //一直执行，奔腾到老不停歇
+            Trigger trigger = newTrigger()
+                    //定义name/group
+                    .withIdentity("trigger1", "group1")
+                            //一旦加入scheduler，立即生效
+                    .startNow()
+                            //使用SimpleTrigger
+                    .withSchedule(simpleSchedule()
+                            //每隔一秒执行一次
+                            .withIntervalInSeconds(6)
+                                    //一直执行，奔腾到老不停歇
+                            .repeatForever())
                     .build();
 
             //定义一个JobDetail
-            JobDetail job = newJob(HelloQuartz.class) //定义Job类为HelloQuartz类，这是真正的执行逻辑所在
-                    .withIdentity("job1", "group1") //定义name/group
-                    .usingJobData("name", "quartz") //定义属性
+            //定义Job类为HelloQuartz类，这是真正的执行逻辑所在
+            JobDetail job = newJob(HelloQuartz.class)
+                    //定义name/group
+                    .withIdentity("job1", "group1")
+                            //定义属性
+                    .usingJobData("name", "quartz")
                     .build();
 
             //加入这个调度
